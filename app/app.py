965ea516381aeb397cfb81df374375c2fb809a82
tonantr <tonantruong@gmail.com>
2025-02-08 09:31:05 -0500

    Updated index page: clarified login message and improved headings

--- app/app.py
+++ app/app.py
@@ -5,23 +5,24 @@ app = Flask(__name__)
 
 db_handler = DatabaseHandler()
 
-@app.route('/')
+
+@app.route("/")
 def index():
     try:
-        return render_template('index.html')
+        return render_template("index.html")
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-@app.route('/login')
+
+@app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
-@app.route('/register') 
+
+@app.route("/register")
 def register():
-    return render_template('register.html')
+    return render_template("register.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-        
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import webbrowser
 from app.app import app
 
 if __name__ == '__main__':
+    webbrowser.open("http://127.0.0.1:5000")
     app.run(debug=True)
