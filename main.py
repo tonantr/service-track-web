416f48999bf9da57aaa9a05331242840c11498e7
tonantr <tonantruong@gmail.com>
2025-02-09 14:46:11 -0500

    Change title to 'CSM' in HTML files

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
 from app.app import app
 
-if __name__ == '__main__':
-    
-    subprocess.Popen(["/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", "http://127.0.0.1:5000/"])
+if __name__ == "__main__":
 
     app.run(debug=True)
