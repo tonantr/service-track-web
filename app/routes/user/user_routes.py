300595c30c8f64185d951125f0e39c1e7a73920a
tonantr <tonantruong@gmail.com>
2025-02-14 15:38:55 -0500

    added get_cars method in user_routes

--- app/routes/user/user_routes.py
+++ app/routes/user/user_routes.py
@@ -50,3 +50,24 @@ def init_app(app):
             logged_in_user=session["username"],
             role=session["role"],
         )
+
+    @app.route("/user/cars")
+    def get_cars():
+        if not Helpers.check_user_session():
+            return redirect(url_for("index"))
+        
+        try:
+            user = helpers.get_user_by_username(session["username"])
+            if not user:
+                flash(ERROR_USER_NOT_FOUND, "danger")
+                return redirect(url_for("index"))
+
+            cars = user_actions.get_cars_by_user_id(user["user_id"])
+            if cars is None:
+                cars = []
+
+            return render_template("user/user_cars.html", cars=cars)
+        except Exception as e:
+            logging.error(f"Error occurred: {str(e)}") 
+            error_message = f"An error occurred: {str(e)}"
+            return render_template("error.html", error_message=error_message)
